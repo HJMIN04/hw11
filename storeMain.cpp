88e26aa372453f64afbc1e1c888fa9e31644fd9d
HJMIN04 <162969857+HJMIN04@users.noreply.github.com>
2024-05-17 00:09:59 +0900

    Add files via upload

---
+++ storeMain.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "store.h"
+using namespace std;
+int main() {
+	store A, B;
+	B = store(3, 4, 5, 6, 100, 200, 300, 400);
+	cout << A.total() << endl;
+	cout << B.total() << endl;
+	cout << " A mango " << A.mango.price << endl;
+	cout << " B mango " << B.mango.price << endl;
+
+	return 123;
+}
